7ce6c099c6a6bf8a89813be26d978917cbc3a5c6
Arman Arakelyan <armarake@c2r11s6.42yerevan.am>
2025-05-20 17:47:28 +0400

    leaks fixed

--- execute/handle_redirs.c
+++ execute/handle_redirs.c
@@ -6,7 +6,7 @@
 /*   By: armarake <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 15:29:49 by nasargsy          #+#    #+#             */
-/*   Updated: 2025/05/20 17:30:55 by armarake         ###   ########.fr       */
+/*   Updated: 2025/05/20 17:45:17 by armarake         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,14 @@ int	except_here_doc(t_tokens *tokens, int *saved_in, int *saved_out)
 	return (0);
 }
 
+static void	free_values(char *temp, char *input)
+{
+	free(temp);
+	free(input);
+	temp = NULL;
+	input = NULL;
+}
+
 void	here_doc(t_tokens *tokens)
 {
 	char	*input;
@@ -59,17 +67,21 @@ void	here_doc(t_tokens *tokens)
 
 	input = NULL;
 	res = ft_strdup("");
+	if (!res)
+		return ;
 	while (1)
 	{
 		ft_putstr_fd("> ", STDOUT_FILENO);
 		input = get_next_line(STDIN_FILENO);
-		if (!input || !ft_strncmp(input, tokens->next->token, ft_strlen(tokens->next->token)))
+		if (!input || !ft_strncmp(input,
+				tokens->next->token, ft_strlen(tokens->next->token)))
 			break ;
 		temp = res;
 		res = ft_strjoin(res, input);
-		free(temp);
-		temp = NULL;
+		free_values(temp, input);
 	}
+	if (input)
+		free(input);
 	ft_putstr_fd(res, STDIN_FILENO);
 	get_next_line(-1);
 	free(res);
