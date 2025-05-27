89acbee8cdf63b8d8b22d27913ca9f061a5ae1ff
Narek Sargsyan <nasargsy@c2r12s5.42yerevan.am>
2025-05-27 16:56:25 +0400

    Fixed error codes for cd

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: armarake <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 13:12:16 by nasargsy          #+#    #+#             */
-/*   Updated: 2025/05/27 16:49:35 by nasargsy         ###   ########.fr       */
+/*   Updated: 2025/05/27 16:55:48 by nasargsy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ int	cd(t_tokens *tmp, t_hash_table *env)
 		return (0);
 	current = ht_search(env, "PWD");
 	if (!current)
-		return (quit_with_error(1, "cd", "PWD not set", errno));
+		return (quit_with_error(1, "cd", "PWD not set", 1));
 	if (chdir(new) == -1)
-		return (quit_with_error(1, "cd", NULL, errno));
+		return (quit_with_error(1, "cd", NULL, 1));
 	new = getcwd(NULL, 0);
 	if (!new || !*new)
 		return (0);
